9bb8ef4fba6dc94c3319df466656b5b9414fca6b
Delio <delioda@yahoo.it>
2015-06-23 00:39:25 +0100

    Improved the variable type expression. It accepts nested objects and objects nested inside lists, but it does not accept yet lists inside lists

--- lib/expressions/variable.dart
+++ lib/expressions/variable.dart
@@ -5,10 +5,62 @@ class _EDVariable implements _EDExpressionType{
   _EDVariable(this.name);
 
   String parse(Map data) {
-    if (!data.containsKey(name)) {
+    String key;
+    int keyLength = name.indexOf(new RegExp(r'(\.|\[)'));
+    if (keyLength >= 0) {
+      key = name.substring(0, keyLength);
+    } else {
+      key = name;
+    }
+    print(data);
+    if (!data.containsKey(key)) {
       throw 'Variable ' + name + ' has not be provided';
     }
+    if (keyLength >= 0) {
+      if (data[key] is Map) {
+        return _getFromMap(name.substring(keyLength), data[key]);
+      }
+
+      if (data[key] is List) {
+        return _getFromArray(name.substring(keyLength), data[key]);
+      }
+    }
     return data[name].toString();
+  }
+
+  String _getFromMap(String value, Map data) {
+    if (value[0] != '.') {
+      throw 'Expected an object property';
+    }
+    _EDVariable subKey = new _EDVariable(value.substring(1));
+    return subKey.parse(data);
+  }
+
+
+  String _getFromArray(String value, List data) {
+    if (value[0] != '[') {
+      throw 'Expected an array key';
+    }
+    int idx = int.parse(value.substring(1,value.indexOf(']')), onError: (source) => -1);
+
+    if (idx == -1) {
+      throw 'Expected an integer, found' + value.substring(1,value.indexOf(']'));
+    }
+
+    if (data.length <= idx) {
+      throw 'The index ' + idx.toString() + ' is out of range';
+    }
+
+    if(value.indexOf(']') == (value.length-1)) {
+      return data[idx].toString();
+    }
+
+    int cursor = value.indexOf(']')+1;
+    if (value[cursor] == '.') {
+      cursor++;
+    }
+    _EDVariable subKey = new _EDVariable(value.substring(cursor));
+    return subKey.parse(data[idx]);
   }
 
 }
--- web/demo1.dart
+++ web/demo1.dart
@@ -3,5 +3,11 @@ import 'package:easydr/easydr.dart';
 main() async {
 
   var myTemplate = new EDTemplate('./TestTemplate.html');
-  print(myTemplate.parse({'myVar': 1, 'tests': ['a', 'b', 'c']}));
+  print(myTemplate.parse({
+    'myVar': 1,
+    'tests': ['a', 'b', 'c'],
+    'myVar2': {'a' : 5, 'b': {'c': 'subObject'}},
+    'myVar3': ['x','z', 'y'],
+    'myVar4': ['x',{'b' : 'test'}, ['subA', 'subB']]
+  }));
 }
